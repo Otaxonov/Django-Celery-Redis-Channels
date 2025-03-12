6e13f313246a59c3f38d68a697d46a1c7dcce787
Otaxonov <Otaxonov_98@mail.ru>
2025-03-13 02:15:58 +0500

    Jokes App

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-python manage.py makemigrations
-python manage.py migrate
-
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@gmail.com', 'admin')" \
-
-python manage.py collectstatic --no-input
-
-python manage.py runserver 0.0.0.0:8000
